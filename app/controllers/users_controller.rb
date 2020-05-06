df21d67c0c3a893b757ec371b67a5f2984f9f2be
Selim Ozdogan <selimozdogan@hotmail.com>
2020-05-06 01:13:52 -0700

    added User update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -14,9 +16,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+      redirect_to posts_path
+    else
+      render :edit
+    end
+
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
 
   resources :posts do
     resources :comments, shallow: true, only: [:create, :destroy]
